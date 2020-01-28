f1af9649b370793e184d54bccafd1b95fe815164
chhotu1 <chhotukumarsow@gmail.com>
2020-01-28 07:58:48 +0530

    update

--- myProject/settings.py
+++ myProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gfg88&qgwe6pokbu+3iv#)fkusf^xyqebqt@xg@ro3-1=z#!hh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
